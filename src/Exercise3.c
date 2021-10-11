6da5d640915b6743f4182dd7aeeb4d483f592d53
PhuongUyenITITIU20348 <ititiu20348@student.hcmiu.edu.vn>
2021-10-11 16:56:29 +0700

    Ex3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -10,25 +10,22 @@ ______________________________________
 #include <stdlib.h>
 #include <math.h>
 
-int main () {
-	int n;
-	int a = 0;
-	printf("n: ");
-	scanf("%d", &n);
-	if (n == 0 || n == 1) {
-		printf("%d", n);
-	}
-	else {
-		for (int i = 2; i <= n; i++) {
-			while (n % i == 0) {
-				printf("%d", i);
-				n /= i;
-			}
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	int testcase = atoi(argv[1]);
+	
+	//Your codes here
+	int arr[20] = {2, 3, 5, 7, 11, 13, 19, 23, 29, 31, 37, 41, 43, 47, 53, 57, 59, 61, 69};
+	int i = 0;
+	while ((testcase != 1) && (i <= 19)){
+		while(testcase % arr[i] == 0){
+			if (testcase / arr[i] != 1) printf("%d * ", arr[i]);
+			else printf("%d", arr[i]);
+			testcase /= arr[i];
 		}
-	}				
- 
-	
-	
+	i++;
+	}
 	
 	return 0;
 }
+
